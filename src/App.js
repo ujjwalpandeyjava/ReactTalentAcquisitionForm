e3216cf7a49c890e8ab983df78dcf4cb9e424f37
Ujjwal Pandey <ujjwalpandey.aps@gmail.com>
2023-11-19 23:43:59 +0530

    Added build optimize

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import BasicNav from './BasicNav'
 import FormCompletionProgress from './FormCompletion'
